13e3ade3238ccfc4d30ffb5f64addc88c1e431d1
hemijing <hehuan_work@126.com>
2019-05-11 15:01:49 +0800

    Update NettyTest.java

--- src/main/java/NettyTest.java
+++ src/main/java/NettyTest.java
@@ -1,6 +1,7 @@
-
+    
 public class NettyTest {
     public static void main(String[] args) {
         System.out.println("===========================");
+        System.out.println("My Netty Test...");
     }
 }
